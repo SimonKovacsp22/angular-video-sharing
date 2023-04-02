f33ea0f96295e932c4fcd95297668406c3d01151
SimonKovacsp22 <postcardsent@gmail.com>
2023-04-02 19:24:39 +0200

    add form validation and alterts

--- clips/src/app/user/login/login.component.ts
+++ clips/src/app/user/login/login.component.ts
@@ -3,8 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  credentials = {
+    email: '',
+    password: '',
+  };
 
+  showAlert = false;
+  alertMsg = 'Please wait! Your account is being created.';
+  alertColor = 'blue';
+  login() {
+    this.showAlert = true;
+    this.alertMsg = 'Your cradentials are being authenticated.';
+    this.alertColor = 'blue';
+  }
 }
