65f6eb738bd55c529bbd9cf7757e8eea20b36d09
SimonKovacsp22 <postcardsent@gmail.com>
2023-04-10 12:02:55 +0200

    not safe for work

---
+++ clips/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDEBchde6IMRTMvYTRl0hdq-ulxMxUlwok',
+    authDomain: 'clips-c6f16.firebaseapp.com',
+    projectId: 'clips-c6f16',
+    storageBucket: 'clips-c6f16.appspot.com',
+    messagingSenderId: '295451548980',
+    appId: '1:295451548980:web:cda7e29c8bb9d1ef4f9961',
+  },
+};
